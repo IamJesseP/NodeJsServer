8102b521ef06488a2f8d9db11cb08588c8929823
Jesse <jesseorhs@gmail.com>
2023-03-12 13:28:50 -0700

    Add error page, dummy image to home page

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const server = http.createServer((req, res) => {
     const url = req.url
     let tableData = "<table border='1'><tr><th>ID</th><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone Number</th></tr>"
     if (url === '/'){
-        res.write("Home Page")
+        res.write("<h1>Welcome to the home page!<h1><img src='https://dummyimage.com/600x400/000/fff'>")
         res.end()
     }
-    if (url === '/message'){
-        res.write("Welcome to my message page")
-        res.end()
-    }
-    if (url === '/list'){
+    else if (url === '/list'){
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
             .then(data => {
@@ -21,7 +17,9 @@ const server = http.createServer((req, res) => {
                 res.end()
             })
     }
-
+    else {
+        res.write("Error 404 Page Not Found")
+    }
     function createData(data){
         data.forEach(element => {
             tableData+=`<tr><td>${element.id}</td><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td><td>${element.address.street}</td><td>${element.phone}</td></tr>`
